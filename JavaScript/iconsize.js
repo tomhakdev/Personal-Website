c29444c4ff9d6f1c9a86152e424980eb9b30dfa6
tomhakdev <134178320+tomhakdev@users.noreply.github.com>
2024-01-02 15:34:20 -0500

    changes and such

--- JavaScript/iconsize.js
+++ JavaScript/iconsize.js
@@ -1,15 +1,16 @@
 window.addEventListener("scroll", function () {
-    const icons = document.querySelectorAll(".scroll-link i");
-  
-    icons.forEach((icon) => {
+  const icons = document.querySelectorAll(".scroll-link i");
+
+  icons.forEach((icon) => {
       const sectionId = icon.parentNode.getAttribute("href");
       const section = document.querySelector(sectionId);
-      const sectionOffset = section.offsetTop + 250; 
-  
-      if (window.scrollY >= sectionOffset) {
-        icon.classList.add("icon-active");
+      const rect = section.getBoundingClientRect();
+
+      // Check if the section is currently filling the entire viewport
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+          icon.classList.add("icon-active");
       } else {
-        icon.classList.remove("icon-active");
+          icon.classList.remove("icon-active");
       }
-    });
   });
+});
